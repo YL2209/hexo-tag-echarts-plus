530505c6dc5e7701bcf235104bc2473844bfdd36
YL2209 <3021027861@qq.com>
2023-11-25 13:55:47 +0800

    npm publish

---
+++ README.md
@@ -0,0 +1,44 @@
+# hexo-tag-echarts-plus
+
+[![npm](https://unpkg.com/hexo-tag-echarts-plus/lib/assets/hexo-tag-echarts-plus.svg)]() 
+
+本插件基于[Konwbase](https://github.com/knowiki)的[hexo-tag-echarts4](https://github.com/knowiki/hexo-tag-echarts4)修改。
+
+Insert [Echarts](https://echarts.apache.org/handbook/zh/get-started/) in Hexo by using tags.
+
+## 安装
+
+1. 安装插件,在博客根目录`[Blogroot]`下打开终端，运行以下指令：
+
+```bash
+npm install hexo-tag-echarts-plus --save
+```
+
+2. 添加配置信息，以下为写法示例
+  在站点配置文件`_config.yml`或者主题配置文件`_config.butterfly.yml`中添加
+
+```yaml
+# tag_echarts-plus
+# see https://naokuo.top/posts/336fb21e/
+tag_echarts:
+  enable: true # 开关
+  priority: 5 #过滤器优先权
+  CDN:
+    tag_echarts_js: https://npm.elemecdn.com/echarts@5.4.3/dist/echarts.min.js
+```
+
+3. 参数释义
+
+  |参数|备选值/类型|释义|
+  |:--|:--|:--|
+  |enable|true/false|【必选】控制开关|
+  |priority|number|【可选】过滤器优先级，数值越小，执行越早，默认为10，选填|
+  |CDN.tag_echarts_js|URL|【可选】echarts标签的JS依赖，为避免CDN缓存延迟，建议将@latest改为具体版本号|
+
+## Echarts外挂标签文档
+1. [Tag Echarts](https://naokuo.top/posts/336fb21e/)
+
+2. Echarts [官方实例](https://echarts.apache.org/handbook/zh/get-started/)
+
+
+
---
+++ index.js
@@ -0,0 +1,50 @@
+'use strict'
+
+const urlFor = require('hexo-util').url_for.bind(hexo)
+
+hexo.extend.filter.register('after_generate', function (locals) {
+  // 首先获取整体的配置项名称
+  const config = hexo.config.tag_echarts || hexo.theme.config.tag_echarts
+  // 如果配置开启
+  if (!(config && config.enable)) return
+  // 集体声明配置项
+  const data = {
+    echartsCDN: config.CDN.tag_echarts_js ? urlFor(config.CDN.tag_echarts_js) : 'https://npm.elemecdn.com/echarts@5.4.3/dist/echarts.min.js'
+  }
+  //cdn资源声明
+  //head引入资源
+  const echarts_js = `<script src="${data.echartsCDN}"></script>`
+  // 注入脚本资源
+  if (data.echartsCDN) {
+    hexo.extend.injector.register('head_end', echarts_js, "default")
+  }
+
+},
+  hexo.extend.helper.register('priority', function () {
+    // 过滤器优先级，priority 值越低，过滤器会越早执行，默认priority是10
+    const pre_priority = hexo.config.tag_echarts.priority ? hexo.config.tag_echarts.priority : hexo.theme.config.tag_echarts.priority
+    const priority = pre_priority ? pre_priority : 10
+    return priority
+  })
+)
+
+function echartsMaps(args, content) {
+  args = args.join(' ').split(',')
+  const Id = ((Math.random() * 9999) | 0)
+  const Width = args[0] ? args[0] : '85%'
+  const Height = args[1] ? args[1] : '400'
+  const Mode = args[2] ? args[2] : 'null'
+  const Renderer = args[3] ? args[3] : 'svg'
+  // 布局
+  let result = '';
+  result += '<div id="echarts_' + Id + '" style="width: ' + Width + ' ;height: ' + Height + 'px;"></div>';
+  result += '<script type="text/javascript">';
+  result += 'var echarts_myChart' + Id + ' = echarts.init(document.getElementById("echarts_' + Id + '"), ' + Mode + ', { renderer: \'' + Renderer + '\' });';
+  result += '' + content + '';
+  result += 'echarts_myChart' + Id + '.setOption(option)';
+  result += '</script>';
+  return result;
+}
+
+hexo.extend.tag.register('图表', echartsMaps, { ends: true })
+hexo.extend.tag.register('echarts', echartsMaps, { ends: true })
